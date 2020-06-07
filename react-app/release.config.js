06dd942b89ce5ef1d6eb597506fe7ae94c4d0d00
digital ankit <akjaph@gmail.com>
2020-06-08 03:04:32 +0530

    installing semantic release in our project

---
+++ react-app/release.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+	branches: "master",
+	repositoryUrl: "https://github.com/alialaa/react-app",
+	plugins: [
+		"@semantic-release/commit-analyzer", "@semantic-release/release-notes-generator", 
+		"@semantic-release/github"
+		]
+}
