34d282dea87fcadc8b2931f773208a6691fb10bf
digitalankit <akjaph@gmail.com>
2020-06-22 12:59:09 +0530

    fix: creating a simple javascript action

---
+++ react-app/.github/actions/hello/index.js
@@ -0,0 +1,15 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+try {
+	// throw( new Error("some error message"));
+	const name = core.getInput('who-to-greet');
+	console.log(`Hello ${name}`);
+
+	const time = new Date();
+	core.setOutput("time", time.toTimeString());
+
+	console.log(JSON.stringify(github, null, '\t'));
+} catch(error) {
+	core.setFailed(error.message);
+}
