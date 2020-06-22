8e9bcb4dba137dcf106af780e96c45ec070b9255
digitalankit <akjaph@gmail.com>
2020-06-22 17:50:47 +0530

    fix: lets discover more about the github core package

--- react-app/.github/actions/hello/dist/index.js
+++ react-app/.github/actions/hello/dist/index.js
@@ -2422,16 +2422,26 @@ const core = __webpack_require__(470);
 const github = __webpack_require__(469);
 
 try {
-	// throw( new Error("some error message"));
-	const name = core.getInput('who-to-greet');
-	console.log(`Hello ${name}`);
+  //   throw new Error("some error message");
 
-	const time = new Date();
-	core.setOutput("time", time.toTimeString());
+  core.debug("Debug message");
+  core.warning("Warning message");
+  core.error("Error message");
 
-	console.log(JSON.stringify(github, null, '\t'));
-} catch(error) {
-	core.setFailed(error.message);
+  const name = core.getInput("who-to-greet");
+  core.setSecret(name);
+  console.log(`Hello ${name}`);
+
+  const time = new Date();
+  core.setOutput("time", time.toTimeString());
+
+  core.startGroup("Logging github object");
+  console.log(JSON.stringify(github, null, "\t"));
+  core.endGroup();
+
+  core.exportVariable("HELLO", "hello");
+} catch (error) {
+  core.setFailed(error.message);
 }
 
 /***/ }),
--- react-app/.github/actions/hello/index.js
+++ react-app/.github/actions/hello/index.js
@@ -1,15 +1,25 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
+const core = require("@actions/core");
+const github = require("@actions/github");
 
 try {
-	throw( new Error("some error message"));
-	const name = core.getInput('who-to-greet');
-	console.log(`Hello ${name}`);
+  //   throw new Error("some error message");
 
-	const time = new Date();
-	core.setOutput("time", time.toTimeString());
+  core.debug("Debug message");
+  core.warning("Warning message");
+  core.error("Error message");
 
-	console.log(JSON.stringify(github, null, '\t'));
-} catch(error) {
-	core.setFailed(error.message);
+  const name = core.getInput("who-to-greet");
+  core.setSecret(name);
+  console.log(`Hello ${name}`);
+
+  const time = new Date();
+  core.setOutput("time", time.toTimeString());
+
+  core.startGroup("Logging github object");
+  console.log(JSON.stringify(github, null, "\t"));
+  core.endGroup();
+
+  core.exportVariable("HELLO", "hello");
+} catch (error) {
+  core.setFailed(error.message);
 }
